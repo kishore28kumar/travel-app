5ca514736519a85794cec3626e53207950b5a78b
kishore28kumar <kishore28kks@gmail.com>
2024-05-19 13:52:14 +0530

    chore: chg meta title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Travel",
+  title: "Hilink",
   description: "Travel UI/UX for Camping",
 };
 
